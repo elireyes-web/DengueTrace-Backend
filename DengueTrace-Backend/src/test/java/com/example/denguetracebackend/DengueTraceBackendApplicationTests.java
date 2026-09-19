2fe94bd89253cf214842cc8294e8491462f0f2b3
ReyesEB <eli.reyes@utec.edu.pe>
2026-09-18 22:37:37 -0500

    chore: Configurando Las Bases del Proyecto

---
+++ DengueTrace-Backend/src/test/java/com/example/denguetracebackend/DengueTraceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.denguetracebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DengueTraceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
